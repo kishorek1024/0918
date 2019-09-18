989b827ef3bf1908a9ba8c00e83888a83a121014
DELL <DELL@DESKTOP-BPC4G7D>
2019-09-18 08:32:41 +0530

    Relese1

---
+++ WebDriverLatest/src/packonetwo/FirstClass.java
@@ -0,0 +1,55 @@
+package packonetwo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\DELL\\Desktop\\8AMWD\\Jars\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("http://facebook.com");
+		driver.manage().window().maximize();
+		
+		/*driver.findElement(By.id("email")).sendKeys("Kishore");
+		driver.findElement(By.id("email")).clear();
+		driver.findElement(By.id("email")).sendKeys("abcdef");
+		driver.findElement(By.id("email")).clear();*/
+		
+		WebElement uid=driver.findElement(By.id("email"));
+		uid.sendKeys("kishore");
+		uid.clear();
+		uid.sendKeys("sfdsdfs");
+		
+		WebElement pwd=driver.findElement(By.id("pass"));
+		pwd.sendKeys("sdfsdf");
+		pwd.clear();
+		/*driver.findElement(By.id("email")).sendKeys("Kishore");
+		driver.findElement(By.id("pass")).sendKeys("abcdef");
+		
+		driver.findElement(By.id("day")).sendKeys("24");
+		driver.findElement(By.id("month")).sendKeys("Apr");
+		
+		driver.findElement(By.id("pass")).clear();
+		
+		driver.findElement(By.id("u_0_9")).click();
+*/
+		
+		/*System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Desktop\\8AMWD\\Jars\\chromedriver.exe");
+		WebDriver driver1=new ChromeDriver();
+		driver1.get("http://facebook.com");
+		driver1.manage().window().maximize();*/
+		
+		/*System.setProperty("webdriver.ie.driver", "C:\\Users\\DELL\\Desktop\\8AMWD\\Jars\\IEDriverServer.exe");
+		WebDriver driver2=new InternetExplorerDriver();
+		driver2.get("http://facebook.com");
+		driver2.manage().window().maximize();*/
+
+	}
+
+}
